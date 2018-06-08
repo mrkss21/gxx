4b694197fb1d4cdc8b9575d3dfcb8e18d00c900e
mrkss21 <34880782+mrkss21@users.noreply.github.com>
2018-06-08 21:59:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,12 @@ GEN_PASS=`pwgen -1 20 -n`
 IP_ADD=`curl ipinfo.io/ip`
 echo -e "rpcuser=keycorpc\nrpcpassword=${GEN_PASS}\nserver=1\nlisten=1\nmaxconnections=256\ndaemon=1\nrpcallowip=127.0.0.1\nexternalip=${IP_ADD}" > /root/.hashrentalcoincore/hashrentalcoin.conf
 hashrentalcoind -daemon
-sleep 10
+sleep 20
 masternodekey=$(hashrentalcoin-cli masternode genkey)
 hashrentalcoin-cli stop
 echo -e "masternode=1\nmasternodeprivkey=$masternodekey" >> /root/.hashrentalcoincore/hashrentalcoin.conf
+echo "Wait please..."
+sleep 45
 hashrentalcoind -daemon
 echo "Your Masternode IP address: ${IP_ADD}:10773"
 echo "Masternode private key: $masternodekey"
